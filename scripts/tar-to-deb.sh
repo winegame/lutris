fe4ba3d71fb399095d13e79a39ec994df51fad3c
SwimmingTiger <hu60.cn@gmail.com>
2020-10-28 13:47:15 +0800

    tar转deb脚本: 把压缩包文件名直接作为包名，不再从中提取版本号

--- scripts/tar-to-deb.sh
+++ scripts/tar-to-deb.sh
@@ -10,6 +10,8 @@ debDir="$2"
 BASE_PACKAGE_NAME="net.winegame.tar"
 MAINTAINER="老虎会游泳 <admin@winegame.net>"
 HOMEPAGE="https://winegame.net"
+VERSION="1.0.0"
+ARCH="amd64"
 
 if [ "$tar" = "" ] || [ "$debDir" = "" ] || ! [ "$UID" = "0" ]; then
     echo -e "Usage:\n\tsudo $0 <path-of-tar> <dir-to-save-deb>"
@@ -18,24 +20,16 @@ if [ "$tar" = "" ] || [ "$debDir" = "" ] || ! [ "$UID" = "0" ]; then
 fi
 
 typeset -l basename
-basename=`basename "$tar"`
-
-arch="amd64"
-if ! [ "$(echo "$basename" | grep -P 'i[36]86' -o)"  = "" ]; then
-    arch="i386"
-fi
-
-basename=`echo "$basename" | sed 's/-x86_64//g' | sed 's/-i[36]86//g' | sed 's/.tar.[xg]z//g'`
-appName=`echo "$basename" | grep -P '^[a-z][a-z0-9]+(-[a-z][a-z0-9]+)*' -o`
-version=`echo "$basename" | grep -P '^[a-z][a-z0-9]+(-[a-z][a-z0-9]+)*-\K([0-9.]+)' -o || echo 1.0.0`
+basename=`basename "$tar" | sed 's/.tar.[xg]z$//g'`
+appName=`echo "$basename" | sed 's/-x86_64//g' | sed 's/-i[3456]86//g'`
 
 rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR/extract/DEBIAN/"
 
 cat <<EOF > "$TMP_DIR/extract/DEBIAN/control"
 Package: $BASE_PACKAGE_NAME.$appName
-Version: $version
-Architecture: $arch
+Version: $VERSION
+Architecture: $ARCH
 Maintainer: $MAINTAINER
 Depends: tar
 Homepage: $HOMEPAGE
@@ -51,11 +45,11 @@ echo -e "Extract tar to:\n\t$TMP_DIR/extract/"
 tar xf "$tar" -C "$TMP_DIR/extract"
 
 echo "Repacking..."
-dpkg-deb -b "$TMP_DIR/extract/" "$debDir/$appName-$version-$arch.deb"
+dpkg-deb -b "$TMP_DIR/extract/" "$debDir/$basename.deb"
 
 echo "Cleaning..."
 echo -e "\trm -rf $TMP_DIR/"
 rm -rf "$TMP_DIR/"
 
 echo -n -e "Package:\n\t"
-ls -lh "$debDir/$appName-$version-$arch.deb"
+ls -lh "$debDir/$basename.deb"
