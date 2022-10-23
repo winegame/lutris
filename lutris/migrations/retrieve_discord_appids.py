88a206300320c7061e69a40b2bb55b4a8d65f151
SwimmingTiger <hu60.cn@gmail.com>
2022-10-23 21:16:19 +0800

    修复更新 discord_id 时的 KeyError 异常

--- lutris/migrations/retrieve_discord_appids.py
+++ lutris/migrations/retrieve_discord_appids.py
@@ -14,13 +14,16 @@ def migrate():
     # Retrieve game data
     games = get_api_games(slugs_to_update)
     for game in games:
-        if not game['discord_id']:
-            continue
+        try:
+            if not game['discord_id']:
+                continue
 
-        sql.db_update(
-            settings.PGA_DB,
-            "games",
-            {"discord_id": game['discord_id']},
-            {"slug": game['slug']}
-        )
-        logger.info("Updated %s", game['name'])
+            sql.db_update(
+                settings.PGA_DB,
+                "games",
+                {"discord_id": game['discord_id']},
+                {"slug": game['slug']}
+            )
+            logger.info("Updated %s", game['name'])
+        except KeyError:
+            pass
